8ee474e33e2b80d37c208b0bcf0e368461f706af
Dániel Gulyás <daniel.gulyas@voiceworks.com>
2016-11-10 15:58:22 +0100

    Added room leave and enter events, integtest for them.

--- lib/potion/channel/room.ex
+++ lib/potion/channel/room.ex
@@ -9,7 +9,7 @@ defmodule Potion.Channel.Room do
   def join("room:" <> room_name, %{"nick" => nick_name}, socket) when is_binary(nick_name) do
     Logger.debug("User joining room #{room_name} with nick: #{nick_name}")
 
-#    send(self, :after_join)
+    send(self, :after_join)
     {:ok, assign(socket, :nick, nick_name)}
   end
 
@@ -18,15 +18,15 @@ defmodule Potion.Channel.Room do
     {:error, %{reason: "Room doesn't exist or message is invalid.'"}}
   end
 
-#  def handle_info(:after_join, socket) do
-#      push(socket, Presence.list(socket))
-#
-#      {:ok, _} = Presence.track(socket, socket.assigns.nickName, %{
-#          online_at: inspect(System.system_time(:seconds))
-#      })
-#
-#      {:noreply, socket}
-#  end
+  def handle_info(:after_join, socket) do
+      push(socket, "presence_state", Presence.list(socket))
+
+      {:ok, _} = Presence.track(socket, socket.assigns.nick, %{
+          online_at: inspect(System.system_time(:seconds))
+      })
+
+      {:noreply, socket}
+  end
 
   def handle_in("message", %{"content" => content}, socket) when is_binary(content) do
     broadcast! socket, "message", %{"content" => content, "sender" => socket.assigns.nick}
--- test/potion/channel/room_test.exs
+++ test/potion/channel/room_test.exs
@@ -8,28 +8,48 @@ defmodule Potion.Channel.RoomTest do
       socket("user_id", %{})
       |> subscribe_and_join(Room, "room:lobby", %{"nick" => nick})
 
+		assert_push("presence_state", _) #asserted presence_state push for the first (setup) user
+		assert_push("presence_diff", _) #asserted presence_diff push for the first (setup) user
     {:ok, socket: socket, nick: nick}
   end
 
-  test "socket should not be able to join without defining it's nick" do
-    sock = socket(0, %{})
-    assert {:error, _} = subscribe_and_join(sock, Room, "room:lobby", %{})
-    assert {:error, _} = subscribe_and_join(sock, Room, "room:lobby", %{:nick => "hi"})
-    assert {:error, _} = subscribe_and_join(sock, Room, "room:lobby", %{"nick" => nil})
+  describe "joining" do
+    test "socket should not be able to join without defining it's nick" do
+			sock = socket(0, %{})
+			assert {:error, _} = subscribe_and_join(sock, Room, "room:lobby", %{})
+			assert {:error, _} = subscribe_and_join(sock, Room, "room:lobby", %{:nick => "hi"})
+			assert {:error, _} = subscribe_and_join(sock, Room, "room:lobby", %{"nick" => nil})
+		end
+
+		test "should be able to join with valid request" do
+			assert {:ok, _, _} = socket(0, %{}) |> subscribe_and_join(Room, "room:lobby", %{"nick" => "daniel"})
+		end
   end
 
-  test "should be able to join with valid request" do
-    assert {:ok, _, _} = socket(0, %{}) |> subscribe_and_join(Room, "room:lobby", %{"nick" => "daniel"})
+  describe "message" do
+		test "should receive an error when pushing invalid messages", %{socket: socket} do
+    	push(socket, "message", nil) |> assert_reply(:error)
+    end
+
+    test "the sent message should be broadcasted with the sender's nick and the message", %{socket: socket, nick: nick} do
+			message = "hi"
+			push(socket, "message", %{"content" => message})
+
+			assert_broadcast "message", %{"content" => ^message, "sender" => ^nick}
+    end
   end
 
-  test "should receive an error when pushing invalid messages", %{socket: socket} do
-    push(socket, "message", nil) |> assert_reply(:error)
-  end
+  describe "presence" do
+    test "should send a list of participants when joined", %{nick: nick_of_setup_user} do
+			socket("user_id2", %{}) |> subscribe_and_join(Room, "room:lobby", %{"nick" => "dani"})
+			assert_push("presence_state", %{^nick_of_setup_user => _}) #the second one received the setup user's presence
+		end
 
-  test "the sent message should be broadcasted with the sender's nick and the message", %{socket: socket, nick: nick} do
-    message = "hi"
-    push(socket, "message", %{"content" => message})
+		test "should broadcast the event of user leave and enter" do
+			nick = "dani"
+			socket("user_id2", %{}) |> subscribe_and_join(Room, "room:lobby", %{"nick" => nick})
 
-    assert_broadcast "message", %{"content" => message, "sender" => nick}
+			assert_broadcast("presence_diff", %{joins: %{^nick => _}}) #assert that user "dani" is in the join
+		end
   end
 end
