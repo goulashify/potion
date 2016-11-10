6a27c627eefdbf7ae202cc117841d2c68794e3a4
Dániel Gulyás <daniel.gulyas@voiceworks.com>
2016-11-10 16:01:46 +0100

    Prettied code, removed camel cases.

--- lib/potion/channel/room.ex
+++ lib/potion/channel/room.ex
@@ -1,6 +1,5 @@
 defmodule Potion.Channel.Room do
   use Phoenix.Channel
-  # TODO: Figure out how to get push() from Potion.Web without creating one just for this
 
   alias Potion.Channel.Presence
 
@@ -9,22 +8,19 @@ defmodule Potion.Channel.Room do
   def join("room:" <> room_name, %{"nick" => nick_name}, socket) when is_binary(nick_name) do
     Logger.debug("User joining room #{room_name} with nick: #{nick_name}")
 
-    send(self, :after_join)
+    send self, :after_join
     {:ok, assign(socket, :nick, nick_name)}
   end
 
   def join(_room, message, _socket) do
     Logger.warn("User failed to join room, #{inspect(message)}")
-    {:error, %{reason: "Room doesn't exist or message is invalid.'"}}
+    {:error, %{reason: "Room doesn't exist or message is invalid."}}
   end
 
   def handle_info(:after_join, socket) do
-      push(socket, "presence_state", Presence.list(socket))
+      push socket, "presence_state", Presence.list(socket)
 
-      {:ok, _} = Presence.track(socket, socket.assigns.nick, %{
-          online_at: inspect(System.system_time(:seconds))
-      })
-
+      {:ok, _} = Presence.track socket, socket.assigns.nick, %{online_at: inspect(System.system_time(:seconds))}
       {:noreply, socket}
   end
 
@@ -33,8 +29,8 @@ defmodule Potion.Channel.Room do
     {:noreply, socket}
   end
 
-  def handle_in(messageName, messageBody, socket) do
-    Logger.warn("Unmatched message with name \"#{messageName}\" with body #{messageBody}.")
+  def handle_in(name, body, socket) do
+    Logger.warn("Unmatched request with name \"#{name}\" with body #{inspect(body)}.")
     {:reply, {:error, %{reason: "Unmatched call."}}, socket}
   end
 
